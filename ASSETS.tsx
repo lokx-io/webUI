07db1d93cc7c57fedd2c775cbf328233f3ab1a78
support <support@lokx.io>
2024-11-14 20:48:13 +0000

    Initial pov mockup

---
+++ ASSETS.tsx
@@ -0,0 +1,11 @@
+const ASSETS = [
+  { asset: "Child Trust Fund", conditions: "30-JUN-2030", recipient: "$MARK", type: "ADA" },
+  { asset: "Savings Account", conditions: "02-FEB-2027", recipient: "$JOHN", type: "ADA" },
+  { asset: "Will and Testament", conditions: "Deceased", recipient: "$PARTNER", type: "Document" },
+  { asset: "Family Trust Fund", conditions: "Deceased", recipient: "$PARTNER", type: "BTC" },
+  { asset: "Birth Certificate", conditions: "On Demand", recipient: "N/A", type: "Document" },
+  { asset: "Passport", conditions: "On Demand", recipient: "N/A", type: "Document" },
+  { asset: "Driving License", conditions: "On Demand", recipient: "N/A", type: "Document" },
+  { asset: "Identity Card", conditions: "On Demand", recipient: "N/A", type: "Document" },
+  { asset: "Deposit", conditions: "Escrow", recipient: "Released", type: "USDJ" }
+];
--- lokx-web-pov/src/App.tsx
+++ lokx-web-pov/src/App.tsx
@@ -1,26 +1,106 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo-adavault.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function MyWallet() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count+1);
+    alert(`You clicked me ${count} times!`);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={handleClick}>Load wallet</button>
+  );
+}
+
+function AssetRow({ asset }) {
+  return (
+    <tr>
+      <td>{asset.assetName}</td>
+      <td>{asset.assetConditions}</td>
+      <td>{asset.assetRecipient}</td>
+      <td>{asset.assetType}</td>
+    </tr>
+  );
+}
+
+function AssetSortableList({ assets }) {
+  const rows : any[] = [];
+  assets.forEach((asset) => {
+    rows.push(
+      <AssetRow asset={asset} key={asset.assetName} />
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Asset</th>
+          <th>Conditions</th>
+          <th>Recipient</th>
+          <th>Type</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function AssetSearchBar() {
+  return (
+    <div>
+    Icon
+    <form>
+      <input type="text" placeholder="Search..." />
+    </form>
     </div>
   );
 }
 
-export default App;
+function AssetMainDialog({ assets }) {
+  return (
+    <div>
+      <AssetSearchBar />
+      <AssetSortableList assets={assets} />
+    </div>
+  );
+}
+
+function MenuBar() {
+  return (
+    <div>
+      <MyWallet />
+    </div>
+  );
+}
+
+
+const ASSETS = [
+  { assetName: "Child Trust Fund", assetConditions: "30-JUN-2030", assetRecipient: "$MARK", assetType: "ADA" },
+  { assetName: "Savings Account", assetConditions: "02-FEB-2027", assetRecipient: "$JOHN", assetType: "ADA" },
+  { assetName: "Will and Testament", assetConditions: "Deceased", assetRecipient: "$PARTNER", assetType: "Document" },
+  { assetName: "Family Trust Fund", assetConditions: "Deceased", assetRecipient: "$PARTNER", assetType: "BTC" },
+  { assetName: "Birth Certificate", assetConditions: "On Demand", assetRecipient: "N/A", assetType: "Document" },
+  { assetName: "Passport", assetConditions: "On Demand", assetRecipient: "N/A", assetType: "Document" },
+  { assetName: "Driving License", assetConditions: "On Demand", assetRecipient: "N/A", assetType: "Document" },
+  { assetName: "Identity Card", assetConditions: "On Demand", assetRecipient: "N/A", assetType: "Document" },
+  { assetName: "Deposit", assetConditions: "Escrow", assetRecipient: "Released", assetType: "USDJ" }
+];
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <div style={{justifyContent:'center', alignItems:'center', height: '10vh'}}>
+        <MenuBar />
+      </div>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '50vh'}}>
+        <AssetMainDialog assets={ASSETS} />
+      </div>
+    </div>
+  )
+}
